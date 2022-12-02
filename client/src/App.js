d97f1a4e17cdab0aa3c703bdf303fbc1b7c03819
Katie Liu <katieliu59@gmail.com>
2022-12-02 16:43:20 -0500

    Build and style Login page

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,7 @@ import { accessToken, logout, getCurrentUserProfile } from './Spotify.js';
 import { catchErrors } from './utils';
 import { GlobalStyle } from './styles';
 import styled from 'styled-components/macro';
-
-const StyledLoginButton = styled.a`
-    background-color: var(--green);
-    color: var(--white);
-    padding: 10px 20px;
-    margin: 20px;
-    border-radius: 30px;
-    display: inline-block;
-`;
+import { Login } from './pages';
 
 function App() {
     const [token, setToken] = useState(null);
@@ -34,11 +26,7 @@ function App() {
             <GlobalStyle />
             <header className="App-header">
                 {!token ? (
-                    <StyledLoginButton
-                        className="App-link"
-                        href="http://localhost:8888/login">
-                        Log in to Spotify
-                    </StyledLoginButton>
+                    <Login />
                 ) : (
                     <Router>
                         <Routes>
